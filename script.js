1de329269cc5ef01e5865977adf3f62eeb02d368
Mikayla Rogers <m.ikaylarogers@icloud.com>
2020-04-06 16:06:59 -0400

    new

--- script.js
+++ script.js
@@ -14,10 +14,14 @@ var fortuneButton = document.getElementById("fortune-input");
 var inputAnswer = document.getElementById("fortune-button");
 var outputAnswer = document.getElementById("fortune-output");
 
+var outputIndex;
+var currentOutput = document.getElementById("fortune-outputText");
+
 var answerArray = ["you are blessed", "you are purposed", "you are the head, not the tail", "you are healed", "you are accepted", "you are creative", "you are whole", "you are worth more than gold"];
-var randomIndex = answerArray[Math.floor( Math.random() * answerArray.length)];
 
 fortuneButton.addEventListener("click", tellFortune);
 
 function tellFortune() {
+  outputAnswer = Math.floor(Math.random() * answerArray.length);
+  currentOutput.innerHTML = inputAnswer.value + ": " + answerArray[outputIndex];
 }
