b1214e7586c05878bc55c116952e5b29a01b38dc
Mikayla Rogers <m.ikaylarogers@icloud.com>
2020-04-06 15:38:34 -0400

    layout update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CALCULATOR //
 var halfButton = document.getElementById("half-button");
 var inputNumber = document.getElementById("half-input");
 
@@ -6,4 +7,19 @@ halfButton.addEventListener("click", divideNumber);
 function divideNumber() {
   alert(inputNumber.value / 2);
   console.log("half of" + " " + inputNumber.value + "is" + inputNumber.value / 2);
+}
+
+// FORTUNE //
+var fortuneButton = document.getElementById("fortune-input");
+var inputAnswer = document.getElementById("fortune-button");
+var outputAnswer = document.getElementById("fortune-output");
+
+var answerArray = ["you are blessed", "you are purposed", "you are the head, not the tail", "you are healed", "you are accepted", "you are creative", "you are whole", "you are worth more than gold"];
+var randomIndex = answerArray[Math.floor( Math.random() * answerArray.length)];
+
+fortuneButton.addEventListener("click", tellFortune);
+
+function tellFortune() {
+  
+
 }
